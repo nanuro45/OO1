aaa17506e5f568ca968246bc1d95ab77932e1dee
Nahuel Rodriguez <62100388+nanuro45@users.noreply.github.com>
2022-10-17 16:49:14 -0300

    Update OOBnB.java

--- AlquileresPropiedades/src/main/java/ar/edu/unlp/info/oo1/AlquileresPropiedades/OOBnB.java
+++ AlquileresPropiedades/src/main/java/ar/edu/unlp/info/oo1/AlquileresPropiedades/OOBnB.java
@@ -2,6 +2,7 @@ package ar.edu.unlp.info.oo1.AlquileresPropiedades;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.stream.DoubleStream;
 
 public class OOBnB {
 	private ArrayList<Reserva> reservas;
@@ -17,8 +18,19 @@ public class OOBnB {
 	}
 	public ArrayList<Reserva> buscarPropiedadDisponible(LocalDateTime solicitud){
 		return this.reservas.stream().map(r -> r.includesDate(solicitud)).collect(null);
-		
 	}
-	
-	
+	public void eliminarReserva(LocalDateTime c) {
+		if(buscarPropiedadPorFecha(c) != null) {
+			this.reservas.remove(buscarPropiedadPorFecha(c));
+		}
+	}
+	private Reserva buscarPropiedadPorFecha(LocalDateTime c) {
+		return this.reservas.stream().filter(i -> i.includesDate(c)).findFirst().orElse(null);	
+	}
+	public DoubleStream calcularPrecioReserva(Reserva r) {
+		return this.reservas.stream().mapToDouble(i -> i.precioReservaDias(r));	
+	}
+	public ArrayList<Reserva> reservasUsuario(Usuario u){
+		return this.reservas.stream().filter(i -> i.getInquilino().getDni() == u.getDni()).collect(null);
+	}
 }
